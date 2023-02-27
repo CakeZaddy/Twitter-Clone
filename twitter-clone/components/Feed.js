c8941fa9cf67dbd8ffb05bed03a4a66e58854412
Susudef <Susudef2u@gmail.com>
2023-02-27 11:05:09 +0100

    Framer Motion Animation effect to posts et all

--- twitter-clone/components/Feed.js
+++ twitter-clone/components/Feed.js
@@ -1,6 +1,7 @@
 import { db } from '@/firebase'
 import { SparklesIcon } from '@heroicons/react/outline'
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { AnimatePresence, motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import Input from './Input'
 import Post from './Post'
@@ -15,28 +16,6 @@ export default function Feed() {
       }
     )
   )
-  // const posts = [
-  //   {
-  //     id: '1',
-  //     name: 'Sulaimon Omotosho',
-  //     username: 'Cake_Zaddy',
-  //     userimg:
-  //       'https://res.cloudinary.com/dwgberjxq/image/upload/v1654160150/E1B49A3A-CA3C-48B7-9525-27283210DD86_tbvayq.jpg',
-  //     img: 'https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  //     text: 'Green!!!',
-  //     timestamp: '2 hours ago',
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'Sulaimon Omotosho',
-  //     username: 'Cake_Zaddy',
-  //     userimg:
-  //       'https://res.cloudinary.com/dwgberjxq/image/upload/v1654160150/E1B49A3A-CA3C-48B7-9525-27283210DD86_tbvayq.jpg',
-  //     img: 'https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-  //     text: 'Great!!!',
-  //     timestamp: '1 day ago',
-  //   },
-  // ]
   return (
     <div className='xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl'>
       <div className='flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
@@ -46,9 +25,19 @@ export default function Feed() {
         </div>
       </div>
       <Input />
-      {posts.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      <AnimatePresence>
+        {posts.map((post) => (
+          <motion.div
+            key={post.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <Post key={post.id} post={post} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
 }
