65b0d57889363bfc98a9f446f8a8f2cc827b2256
Susudef <Susudef2u@gmail.com>
2023-02-27 13:49:04 +0100

    install and implement Recoil

--- twitter-clone/components/Post.js
+++ twitter-clone/components/Post.js
@@ -1,3 +1,4 @@
+import { modalState } from '@/atom/modalAtom'
 import { db, storage } from '@/firebase'
 import {
   ChartBarIcon,
@@ -20,11 +21,13 @@ import { deleteObject, ref } from 'firebase/storage'
 import { signIn, useSession } from 'next-auth/react'
 import { useState, useEffect } from 'react'
 import Moment from 'react-moment'
+import { useRecoilState } from 'recoil'
 
 export default function Post({ post }) {
   const { data: session } = useSession()
   const [likes, setLikes] = useState([])
   const [hasLiked, setHasLiked] = useState(false)
+  const [open, setOpen] = useRecoilState(modalState)
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -100,7 +103,10 @@ export default function Post({ post }) {
         {/* icons */}
 
         <div className='flex justify-between text-gray-500 p-2'>
-          <ChatIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
+          <ChatIcon
+            onClick={() => setOpen(!open)}
+            className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100'
+          />
           {session?.user.uid === post?.data().id && (
             <TrashIcon
               onClick={deletePost}
--- twitter-clone/pages/index.js
+++ twitter-clone/pages/index.js
@@ -3,6 +3,7 @@ import { Inter } from '@next/font/google'
 import Sidebar from '@/components/Sidebar'
 import Feed from '@/components/Feed'
 import Widgets from '@/components/Widgets'
+import CommentsModal from '@/components/CommentsModal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -31,7 +32,8 @@ export default function Home({ newsResults, randomUsersResults }) {
           randomUsersResults={randomUsersResults.results}
         />
 
-        {/* Modules */}
+        {/* Modal */}
+        <CommentsModal />
       </main>
     </>
   )
